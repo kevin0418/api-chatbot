09bb4a6e9b5760504cf26bc882b3f93a42d34f90
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2023-12-30 19:19:01 -0800

    Add files via upload

--- api_app.py
+++ api_app.py
@@ -1,19 +1,18 @@
 import os
-import openai
- 
-openai.api_key = os.getenv("OPENAI-API-KEY")
+from openai import OpenAI
 
-print ("OPENAI-API-KEY :", "OPENAI-API-KEY")
-print ("openai.api_key :", openai.api_key)
+client = OpenAI(
+  api_key = os.environ["OPENAI-API-KEY"],
+)
 
-#from openai import OpenAI
 
 from streamlit_chat import message
 
-response  = openai.Completion.create (
+response  = client.completions.create (
   model="text-davinci-003",
   prompt="겨울에 대한 시좀 써줘"  ,
   max_tokens=150  
 )      
  
 print(response.choices[0].text)
+
