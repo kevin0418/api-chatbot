4d38e72210211a19419a2f25a2f607eca04140cc
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2023-12-30 21:52:07 -0800

    Add files via upload

--- api_app.py
+++ api_app.py
@@ -6,13 +6,16 @@ client = OpenAI(
 )
 
 
-from streamlit_chat import message
+#from streamlit_chat import message
 
-response  = client.completions.create (
-  model="text-davinci-003",
-  prompt="겨울에 대한 시좀 써줘"  ,
-  max_tokens=150  
-)      
+response  = client.chat.completions.create (
+  model="gpt-3.5-turbo-1106",
+  messages=[
+      {"role":"system", "content":"You sre a helpful assistance"},
+      {"role":"user", "content":"겨울에 대한 시좀 써줘. json"}
+  ],
+  response_format={"type": "json_object"}
+) 
  
-print(response.choices[0].text)
+print(response.choices[0].message.content)
 
